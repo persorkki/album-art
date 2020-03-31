45728c9c3856bce9d43447d7c63bba937b3697a4
persorkki <herroo@protonmail.com>
2020-03-31 16:53:53 +0300

    added title to images

--- albumart_loader.py
+++ albumart_loader.py
@@ -4,7 +4,7 @@ def get_urls(queryString):
 	res = requests.get(url=f"https://itunes.apple.com/search?term={queryString}&entity=album")
 	imgs = dict()
 	for i in res.json()['results']:
-		imgs[i['artworkUrl100']] = i['artworkUrl100'].replace('100x100bb.jpg', "10000x10000bb.png")
+		imgs[i['collectionName']] = [i['artworkUrl100'].replace('100x100bb.jpg', "10000x10000bb.png"),
+									 i['artworkUrl100'],]
 	return imgs
-
 	
