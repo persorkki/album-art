faabcaa6ec3cb272cc213c0fbc9658699dab135e
persorkki <herroo@protonmail.com>
2020-03-31 17:03:30 +0300

    fixed title only showing first word

--- albumart_loader.py
+++ albumart_loader.py
@@ -4,7 +4,9 @@ def get_urls(queryString):
 	res = requests.get(url=f"https://itunes.apple.com/search?term={queryString}&entity=album")
 	imgs = dict()
 	for i in res.json()['results']:
-		imgs[i['collectionName']] = [i['artworkUrl100'].replace('100x100bb.jpg', "10000x10000bb.png"),
-									 i['artworkUrl100'],]
+		imgs[i['collectionId']] = [i['artworkUrl100'].replace('100x100bb.jpg', "10000x10000bb.png"),
+								   i['artworkUrl100'],
+								   i['collectionName'],]
+								   
 	return imgs
 	
